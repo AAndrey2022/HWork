5e3d0524a81a7a6e37d3b2f7d4f0dc8c58254ed0
andrey <groza001@yandex.ru>
2022-11-05 16:28:14 +0300

    Add solution Task08

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая:
+// 1. на вход принимает число (N), 
+// 2. на выходе показывает все чётные числа от 1 до N.
+
+// Например: 5 -> 2, 4
+//           8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 0) Console.WriteLine("Введено отрицательное число-Некорректный ввод");
+else
+{
+    int count = 2;
+    while (count <= num)
+    {
+        Console.Write($"{count}. ");
+        count+=2;
+    }
+}
+
